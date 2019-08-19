c2ce545e798047894a981f2565bbc9255774d0fb
Aluno <allanfogaca22@gmail.com>
2019-08-19 11:56:56 -0300

    topens

--- src/app/cidade/listar/listar.page.ts
+++ src/app/cidade/listar/listar.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireDatabase } from '@angular/fire/database'
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import {Cidade} from '../entidade/cidade'
 
 @Component({
   selector: 'app-listar',
@@ -6,8 +10,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listar.page.scss'],
 })
 export class ListarPage implements OnInit {
+  listaCidade: Observable<Cidade[]>;
 
-  constructor() { }
+  constructor(private fire: AngularFireDatabase) {
+    this.listaCidade = this.fire.list<Cidade>('cidade').snapshotChanges().pipe(
+      map( lista => lista.map(linha => ({ key: linha.payload.key, ... linha.payload.val() })))
+    );
+  }
+
 
   ngOnInit() {
   }
